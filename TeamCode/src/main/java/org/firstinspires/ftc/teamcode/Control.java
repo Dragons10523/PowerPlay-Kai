964199c5aaeef68d3864f9a7327386d7d064ed09
Dragons 10523 <alaroboticsteam@gmail.com>
2020-10-20 14:35:47 -0700

    fixed gamepad bug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control.java
@@ -4,46 +4,16 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.exception.RobotCoreException;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-public abstract class Control extends LinearOpMode implements Gamepad.GamepadCallback {
+public abstract class Control extends LinearOpMode{
     Thalatte thalatte;
 
     // CONSTANTS
     final double INTAKE = 1.0;
     final double CLAMP = 1.0;
 
-    private ControllerSpec spec = null;
+    public GamepadPrev prev1;
 
-    public GamepadPrev prev1 = new GamepadPrev(gamepad1);
-
-    public GamepadPrev prev2 = new GamepadPrev(gamepad2);
-
-    public interface ControllerSpec {
-
-        enum GamepadId {
-            GAMEPAD_1,
-            GAMEPAD_2,
-        }
-
-        void dpadUp(GamepadId id);
-        void dpadDown(GamepadId id);
-        void dpadLeft(GamepadId id);
-        void dpadRight(GamepadId id);
-        void aButton(GamepadId id);
-        void bButton(GamepadId id);
-        void xButton(GamepadId id);
-        void yButton(GamepadId id);
-        void startButton(GamepadId id);
-        void backButton(GamepadId id);
-        void lBumper(GamepadId id);
-        void rBumper(GamepadId id);
-        void lStickButton(GamepadId id);
-        void rStickButton(GamepadId id);
-
-        void sticks(GamepadId id, float lx, float ly, float rx, float ry);
-
-        void triggers(GamepadId id, float l, float r);
-
-    }
+    public GamepadPrev prev2;
 
     public void drive(double l, double r) {
         thalatte.backRight.setPower(r);
@@ -84,159 +54,12 @@ public abstract class Control extends LinearOpMode implements Gamepad.GamepadCal
 
     public void alize() {
         thalatte = new Thalatte(hardwareMap);
-        try {
-            Gamepad copy = gamepad1;
-            gamepad1 = new Gamepad(this);
-            gamepad1.copy(copy);
-            copy = gamepad2;
-            gamepad2 = new Gamepad(this);
-            gamepad2.copy(copy);
-        } catch(RobotCoreException e) {
-            telemetry.addLine(e.getMessage());
-            telemetry.update();
-        }
-
+        prev1 = new GamepadPrev(gamepad1);
+        prev2 = new GamepadPrev(gamepad2);
     }
 
     public double clamp(double num, double min, double max) {
         return Math.max(Math.min(num, max), min);
-    }
-
-    private ControllerSpec.GamepadId getId(Gamepad gamepad){
-        if(gamepad.id == gamepad1.id){
-            return ControllerSpec.GamepadId.GAMEPAD_1;
-        }
-        return ControllerSpec.GamepadId.GAMEPAD_2;
-    }
-
-    @Override
-    public void gamepadChanged(Gamepad gamepad) {
-        if(opModeIsActive() && spec != null) {
-            switch(getId(gamepad)){
-
-                case GAMEPAD_1:
-
-                    switch(prev1.getEvent()){
-                        case UP:
-                            spec.dpadUp(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case DOWN:
-                            spec.dpadDown(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case LEFT:
-                            spec.dpadLeft(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case RIGHT:
-                            spec.dpadRight(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case A:
-                            spec.aButton(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case B:
-                            spec.bButton(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case X:
-                            spec.xButton(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case Y:
-                            spec.yButton(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case START:
-                            spec.startButton(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case BACK:
-                            spec.backButton(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case LB:
-                            spec.lBumper(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case RB:
-                            spec.rBumper(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case LSB:
-                            spec.lStickButton(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case RSB:
-                            spec.rStickButton(ControllerSpec.GamepadId.GAMEPAD_1);
-                            break;
-                        case NONE:
-                            spec.sticks(ControllerSpec.GamepadId.GAMEPAD_1,
-                                        gamepad.left_stick_x,
-                                       -gamepad.left_stick_y,
-                                        gamepad.right_stick_x,
-                                       -gamepad.right_stick_y);
-                            spec.triggers(ControllerSpec.GamepadId.GAMEPAD_1,
-                                          gamepad.left_trigger,
-                                          gamepad.right_trigger);
-                            break;
-                    }
-
-                    break;
-
-                case GAMEPAD_2:
-
-                    switch(prev2.getEvent()){
-                        case UP:
-                            spec.dpadUp(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case DOWN:
-                            spec.dpadDown(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case LEFT:
-                            spec.dpadLeft(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case RIGHT:
-                            spec.dpadRight(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case A:
-                            spec.aButton(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case B:
-                            spec.bButton(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case X:
-                            spec.xButton(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case Y:
-                            spec.yButton(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case START:
-                            spec.startButton(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case BACK:
-                            spec.backButton(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case LB:
-                            spec.lBumper(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case RB:
-                            spec.rBumper(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case LSB:
-                            spec.lStickButton(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case RSB:
-                            spec.rStickButton(ControllerSpec.GamepadId.GAMEPAD_2);
-                            break;
-                        case NONE:
-                            spec.sticks(ControllerSpec.GamepadId.GAMEPAD_2,
-                                        gamepad.left_stick_x,
-                                       -gamepad.left_stick_y,
-                                        gamepad.right_stick_x,
-                                       -gamepad.right_stick_y);
-                            spec.triggers(ControllerSpec.GamepadId.GAMEPAD_2,
-                                          gamepad.left_trigger,
-                                          gamepad.right_trigger);
-                            break;
-                    }
-
-                    break;
-            }
-        }
-    }
-
-    public void setSpec(ControllerSpec spec) {
-        this.spec = spec;
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 @TeleOp(name = "ShootyShoot")
-public class Test extends Control implements Control.ControllerSpec {
+public class Test extends Control {
 
     double power = 0;
     short negator = 1;
@@ -12,92 +12,34 @@ public class Test extends Control implements Control.ControllerSpec {
     @Override
     public void runOpMode() throws InterruptedException {
         alize();
-        setSpec(this);
         waitForStart();
         while (opModeIsActive()) {
+            GamepadPrev.ButtonEvent ev1;
+
+            do {
+                ev1 = prev1.getEvent();
+                switch(ev1) {
+                    case UP:
+                        power = 1;
+                        break;
+                    case DOWN:
+                        power = 0;
+                        break;
+                    case LEFT:
+                        power = clamp(power-0.2,0.0,1.0);
+                        break;
+                    case RIGHT:
+                        power = clamp(power+0.2,0.0,1.0);
+                        break;
+                    case X:
+                        negator = (short)(-negator);
+                        break;
+                }
+            } while(ev1 != GamepadPrev.ButtonEvent.NONE);
+
             shoot(negator * power);
             telemetry.addData("Shooting Power", negator * power);
             telemetry.update();
         }
-    }
-
-    @Override
-    public void dpadUp(GamepadId id) {
-        power = 1.0;
-    }
-
-    @Override
-    public void dpadDown(GamepadId id) {
-        power = 0.0;
-    }
-
-    @Override
-    public void dpadLeft(GamepadId id) {
-        power = clamp(power + 0.2, 0.0, 1.0);
-    }
-
-    @Override
-    public void dpadRight(GamepadId id) {
-
-    }
-
-    @Override
-    public void aButton(GamepadId id) {
-
-    }
-
-    @Override
-    public void bButton(GamepadId id) {
-
-    }
-
-    @Override
-    public void xButton(GamepadId id) {
-        negator = (short)(-negator);
-    }
-
-    @Override
-    public void yButton(GamepadId id) {
-
-    }
-
-    @Override
-    public void startButton(GamepadId id) {
-
-    }
-
-    @Override
-    public void backButton(GamepadId id) {
-
-    }
-
-    @Override
-    public void lBumper(GamepadId id) {
-
-    }
-
-    @Override
-    public void rBumper(GamepadId id) {
-
-    }
-
-    @Override
-    public void lStickButton(GamepadId id) {
-
-    }
-
-    @Override
-    public void rStickButton(GamepadId id) {
-
-    }
-
-    @Override
-    public void sticks(GamepadId id, float lx, float ly, float rx, float ry) {
-
-    }
-
-    @Override
-    public void triggers(GamepadId id, float l, float r) {
-
     }
 }
