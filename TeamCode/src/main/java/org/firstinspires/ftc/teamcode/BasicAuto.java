5273fbdeaeafd66b1cdec2ec1ac1eb45c3ece0d4
Dragons 10523 <alaroboticsteam@gmail.com>
2021-08-19 15:23:39 -0700

    Added experimental Basic Autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous(name = "Basic Auto")
+public class BasicAuto extends Localization{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        alize();
+        startLocalization(vuFlag.AUTO);
+        vwompClamp(false);
+        waitForStart();
+        stopTfodCrap();
+        drive(0.6, 0.6);
+        ElapsedTime e = new ElapsedTime();
+        while(thalatte.back.getDistance(DistanceUnit.INCH) < 67 && e.seconds() < 1.3);
+        sleep(200);
+    }
+}
