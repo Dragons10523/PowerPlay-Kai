7c5616eda7b88fc6f8de45d4b87b4a832e9e7096
Dragons 10523 <alaroboticsteam@gmail.com>
2021-08-30 15:39:54 -0700

    Added a basic autonomous program which shoots from an off position and then moves forwards.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutoShooting.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous(name = "Basic Auto")
+public class BasicAutoShooting extends Localization{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        alize();
+        startLocalization(vuFlag.AUTO);
+        vwompClamp(false);
+        stopTfodCrap();
+        waitForStart();
+
+        drive(0.7,0.7);
+        sleep(500);
+        drive(-1,-1);
+        sleep(200);
+        drive(0.7,0.7);
+        while(thalatte.back.getDistance(DistanceUnit.INCH) < 5);
+        drive(0,0);
+
+        startTurnTo((Math.PI/2) - 0.5);
+        while(turningFlag) updateLocalization();
+        shoot(1);
+        sleep(500);
+        intake(true);
+        feeder(true);
+        sleep(3000);
+        intake(false);
+        feeder(false);
+        shoot(0);
+        startTurnTo(Math.PI/2);
+        while(turningFlag) updateLocalization();
+        sleep(500);
+
+        drive(0.6, 0.6);
+        ElapsedTime e = new ElapsedTime();
+        while(thalatte.back.getDistance(DistanceUnit.INCH) < 67 && e.seconds() < 1.3);
+        sleep(200);
+    }
+}
