af7fdc3b26316427d6ae90e977c79c999a81c6b3
Andrew Neeley <aneeley05@gmail.com>
2021-09-23 15:53:41 -0700

    asdf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SocketRemote.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SocketRemote.java
@@ -6,31 +6,32 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import java.io.*;
 import java.net.*;
 
+import javax.websocket.OnMessage;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/livedata")
 @TeleOp(name = "SocketRemote", group = "Sensor")
 public class SocketRemote extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        try {
-            ServerSocket serverSocket = new ServerSocket(6969);
-            System.out.println("SocketManager: " + "Waiting for client on port 6969");
-            waitForStart();
-            while (opModeIsActive()) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("SocketManager: " + "Connected client");
-                PrintWriter out =
-                        new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()));
 
-                String inData = in.readLine();
-                out.println("PONG: " + inData);
-                System.out.println("Socket Manager: Client: " + inData);
-            }
+        waitForStart();
+        while(opModeIsActive()) {
 
-            serverSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+    }
+
+    @OnMessage
+    public String handleTextMessage(String message) {
+        System.out.println("New Text Message Received");
+        return message;
+    }
+
+    @OnMessage(maxMessageSize = 1024000)
+    public byte[] handleBinaryMessage(byte[] buffer) {
+        System.out.println("New Binary Message Received");
+        return buffer;
     }
 }
