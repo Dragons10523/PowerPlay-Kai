8e5de4e8e1165f6f47d8c3c4fa643e9a8274e989
Andrew Neeley <aneeley05@gmail.com>
2021-09-24 15:57:16 -0700

    things aboutta brek

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SocketRemote.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SocketRemote.java
@@ -3,13 +3,17 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.Transport;
+import com.corundumstudio.socketio.listener.DataListener;
+
 import java.io.*;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-import javax.websocket.OnMessage;
-import javax.websocket.server.ServerEndpoint;
-
-@ServerEndpoint("/livedata")
 @TeleOp(name = "SocketRemote", group = "Sensor")
 public class SocketRemote extends LinearOpMode {
 
@@ -17,21 +21,46 @@ public class SocketRemote extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         waitForStart();
-        while(opModeIsActive()) {
 
+        // SERVER CODE
+
+        new SocketRemote().listen();
+
+        // END SERVER CODE
+
+    }
+
+
+    public static final int port = 12345;
+    private ServerSocket server;
+
+    public void listen() {
+        try {
+            server = new ServerSocket(4444);
+        } catch (IOException e) {
+            System.out.println("Could not listen on port 4444");
+            System.exit(-1);
         }
+        while(true){
 
-    }
+            try {
+                System.out.println("Waiting for connection");
+                final Socket socket = server.accept();
 
-    @OnMessage
-    public String handleTextMessage(String message) {
-        System.out.println("New Text Message Received");
-        return message;
-    }
+                final InputStream inputStream = socket.getInputStream();
+                final InputStreamReader streamReader = new InputStreamReader(inputStream);
+                BufferedReader br = new BufferedReader(streamReader);
 
-    @OnMessage(maxMessageSize = 1024000)
-    public byte[] handleBinaryMessage(byte[] buffer) {
-        System.out.println("New Binary Message Received");
-        return buffer;
+                // readLine blocks until line arrives or socket closes, upon which it returns null
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+            } catch (IOException e) {
+                System.out.println("Accept failed: 4444");
+                System.exit(-1);
+            }
+        }
     }
 }
