8b0869d661d26087b53a291bc1ef32e3009aca5c
Dragons 10523 <alaroboticsteam@gmail.com>
2021-10-15 16:54:31 -0700

    Added basic robot class Added basic control class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ahi.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+public class Ahi{
+    // Encoder goes in the motor port, A motors for drivetrain and arm motor for manipulation
+    public DcMotor rightA, rightB, leftA, leftB; // Drivetrain
+    public DcMotor arm, succc, capLift; // Manipulation Motors
+    public CRServo ddr; // Manipulation Servos
+    public Servo flup;
+    public BNO055IMU imu;
+
+    public HardwareMap hwmap;
+
+    public WebcamName webcam;
+
+    final boolean drivetrainReverse = false;
+
+    public Ahi(HardwareMap hwmap) {
+        this.hwmap = hwmap;
+
+        rightA = hwmap.get(DcMotor.class, "rightA");
+        rightB = hwmap.get(DcMotor.class, "rightB");
+        leftA = hwmap.get(DcMotor.class, "leftA");
+        leftB = hwmap.get(DcMotor.class, "leftB");
+
+        rightA.setDirection(drivetrainReverse ? DcMotor.Direction.FORWARD : DcMotor.Direction.REVERSE);
+        rightB.setDirection(drivetrainReverse ? DcMotor.Direction.FORWARD : DcMotor.Direction.REVERSE);
+        leftA.setDirection(drivetrainReverse ? DcMotor.Direction.REVERSE : DcMotor.Direction.FORWARD);
+        leftB.setDirection(drivetrainReverse ? DcMotor.Direction.REVERSE : DcMotor.Direction.FORWARD);
+
+        arm = hwmap.get(DcMotor.class, "arm");
+        succc = hwmap.get(DcMotor.class, "intake");
+        flup = hwmap.get(Servo.class, "flup");
+
+        arm.setDirection(DcMotor.Direction.FORWARD);
+        succc.setDirection(DcMotor.Direction.FORWARD);
+
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        capLift = hwmap.get(DcMotor.class, "capLift");
+
+        ddr = hwmap.get(CRServo.class, "ddr");
+
+        BNO055IMU.Parameters parameters             = new BNO055IMU.Parameters();
+        parameters.angleUnit                        = BNO055IMU.AngleUnit.RADIANS;
+        parameters.accelUnit                        = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile              = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled                   = true;
+        parameters.loggingTag                       = "IMU";
+
+        imu = hwmap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        webcam = hwmap.get(WebcamName.class, "Webcam 1");
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public abstract class Control extends LinearOpMode {
+    Ahi ahi;
+
+    final int armOffset = 0;
+
+    enum ArmPosition {
+        START,
+        LOW,
+        MED,
+        HIGH,
+        PICKUP
+    }
+
+    public void drive(double left, double right) {
+        ahi.leftA.setPower(left);
+        ahi.leftB.setPower(left);
+        ahi.rightA.setPower(right);
+        ahi.rightB.setPower(right);
+    }
+
+    public void driveLoop() {
+        double left = ahi.drivetrainReverse ? -gamepad1.right_stick_y : -gamepad1.left_stick_y;
+        double right = ahi.drivetrainReverse ? -gamepad1.left_stick_y : -gamepad1.right_stick_y;
+
+        boolean sneak = gamepad1.left_stick_button || gamepad1.right_stick_button;
+
+        left *= sneak ? 0.3 : 1.0;
+        right *= sneak ? 0.3 : 1.0;
+
+        drive(left, right);
+    }
+
+    public void driveDist() {
+
+    }
+
+    public void armControl(ArmPosition armPosition) {
+        switch(armPosition) {
+            case LOW:
+                ahi.arm.setTargetPosition(3640-armOffset);
+                break;
+            case MED:
+                ahi.arm.setTargetPosition(3300-armOffset);
+                break;
+            case HIGH:
+                ahi.arm.setTargetPosition(2960-armOffset);
+                break;
+            case PICKUP:
+                ahi.arm.setTargetPosition(0-armOffset);
+                break;
+            default:
+                ahi.arm.setTargetPosition(0);
+                break;
+        }
+        ahi.arm.setPower(1.0);
+    }
+
+    public void runIntake(double power) {
+        ahi.succc.setPower(power);
+    }
+
+    public void playDDR(double power) {
+        ahi.ddr.setPower(power);
+    }
+
+    public void setFlup(boolean open) {
+        ahi.flup.setPosition(open ? 1.0 : 0.0);
+    }
+
+    public void setLiftHeight(double power) {
+        ahi.capLift.setPower(power);
+    }
+}
