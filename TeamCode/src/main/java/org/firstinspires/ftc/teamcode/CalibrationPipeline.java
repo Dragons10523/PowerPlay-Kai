42abfaf78ec17f6017dded5a1c878b46fa057071
Dragons 10523 <alaroboticsteam@gmail.com>
2021-11-30 17:27:14 -0700

    Made color calibration much simpler

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalibrationPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalibrationPipeline.java
@@ -16,71 +16,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CalibrationPipeline extends OpenCvPipeline {
-    final double accuracy = 0.01;
-    final int colorPrecision = 5;
-
     Mat lab = new Mat();
-    Mat thresh = new Mat();
-    Mat output = new Mat();
 
     double[] centerColor;
     boolean ready = false;
 
     @Override
     public Mat processFrame(Mat input) {
-        output.release();
-        thresh.release();
-        lab.release();
-
-        output = input.clone();
-        Imgproc.cvtColor(output, lab, Imgproc.COLOR_RGB2Lab);
-        Core.inRange(lab, new Scalar(95, 140, 165), new Scalar(150, 165, 195), thresh);
-
-        //Core.multiply(gray, new Scalar(255), gray);
-
-        //Imgproc.threshold(gray, thresh, 80, 255, Imgproc.THRESH_BINARY); //Isolate bright colors
-
-        Imgproc.morphologyEx(thresh, thresh, Imgproc.MORPH_OPEN, new Mat()); //Apply image pre-processing
-        Imgproc.morphologyEx(thresh, thresh, Imgproc.MORPH_CLOSE, new Mat());
-        Imgproc.GaussianBlur(thresh, thresh, new Size(21.0, 21.0), 0.00);
-
-        List<MatOfPoint> contours = new ArrayList<>();
-        Imgproc.findContours(thresh, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE); //Locate contours
-        Imgproc.drawContours(output, contours, 0, new Scalar(0, 255, 0)); //Draw boxes near contours
+        Imgproc.cvtColor(input, lab, Imgproc.COLOR_RGB2Lab);
 
         centerColor = lab.get((int)lab.rows()/2, (int)lab.cols()/2);
 
-        //output.put((int)output.rows()/2, (int)output.cols()/2, 0, 255, 0, 0);
-
-        input.release();
-        ready = true;
-        return output;
-/*
-        double maxArea = 0;
-        MatOfPoint2f largestQuad = null;
-        for(MatOfPoint contour : contours) {
-            MatOfPoint2f contour2f = new MatOfPoint2f();
-            contour.convertTo(contour2f, CvType.CV_32F); //Convert to MatOfPoint2f
-
-            float epsilon = (float)(accuracy * Imgproc.arcLength(contour2f, true));
-            Imgproc.approxPolyDP(contour2f, contour2f, epsilon, true); //Simplify contours to approximate the shape more simply
-
-
-            Point[] contourArray;
-            contourArray = contour2f.toArray();
-            if(contourArray.length == 4) { //If 4 sided
-                Rect rect = Imgproc.boundingRect(contour2f);
-
-                if(rect.area() > maxArea) { //Save the largest quadrilateral
-                    maxArea = rect.area();
-                    largestQuad = contour2f;
-                }
-            }
-        }
-
-        if(largestQuad == null) return output; //Safety
-
-        return input.mul(largestQuad.inv());*/
+        return lab;
     }
 
     public boolean isReady(){
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraCalibration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraCalibration.java
@@ -7,6 +7,11 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 @Autonomous(name = "CameraCalibration")
 public class CameraCalibration extends LinearOpMode {
     Ahi ahi;
@@ -38,12 +43,19 @@ public class CameraCalibration extends LinearOpMode {
             sleep(50);
         }
 
-        while(opModeIsActive()) {
-            double[] centerColor = calibrationPipeline.getColor();
-            telemetry.addData("Real L*A*B*", (100*centerColor[0]/255) + " " + (centerColor[1]-128) + " " + (centerColor[2]-128));
-            telemetry.addData("OpenCV L*A*B*", (centerColor[0]) + " " + (centerColor[1]) + " " + (centerColor[2]));
-            telemetry.update();
-            sleep(50);
+        double[] centerColor = calibrationPipeline.getColor();
+
+        try {
+            FileOutputStream fos = new FileOutputStream("/storage/emulated/0/FIRST/color.dat");
+
+            byte[] data = ByteBuffer.allocate(8 * 3).putDouble(centerColor[0]).putDouble(centerColor[1]).putDouble(centerColor[2]).array();
+            fos.write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        telemetry.addData("Real L*A*B*", (100*centerColor[0]/255) + " " + (centerColor[1]-128) + " " + (centerColor[2]-128));
+        telemetry.addData("OpenCV L*A*B*", (centerColor[0]) + " " + (centerColor[1]) + " " + (centerColor[2]));
+        telemetry.update();
     }
 }
