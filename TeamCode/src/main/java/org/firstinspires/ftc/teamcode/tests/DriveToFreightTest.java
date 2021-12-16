8376d0e49c04ec18384f4a27f3ed37e895fd08ab
Dragons 10523 <alaroboticsteam@gmail.com>
2021-12-16 09:30:37 -0700

    Added test for driveToFreight()

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DriveToFreightTest.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.AbstractBarcode;
+
+@Autonomous(name = "DriveToFreightTest", group = "Testing")
+public class DriveToFreightTest extends AbstractBarcode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initializeValues();
+        startOpenCV();
+
+        waitForStart();
+
+        driveToFreight();
+        drive(1, 1);
+        runIntake(true);
+        sleep(250);
+        drive(0, 0);
+        runIntake(false);
+    }
+}
