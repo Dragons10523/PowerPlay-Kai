a6def0f8b327fd30759b572a12e690ba0780380d
Dragons 10523 <92609241+Dragons10523@users.noreply.github.com>
2021-12-16 20:16:53 -0700

    Added PIDF control to the motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ahi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ahi.java
@@ -3,8 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -18,15 +20,13 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 
 public class Ahi {
     // Encoder goes in the motor port, A motors for drivetrain and arm motor for manipulation (CH = Control Hub | DH = Driver Hub)
-    public DcMotor rightA, rightB, leftA, leftB; // Drivetrain (CH 0-3)
+    public DcMotorEx rightA, rightB, leftA, leftB; // Drivetrain (CH 0-3)
     public DcMotor arm, succc, capLift; // Manipulation Motors (DH 0-2)
     public CRServo ddr; // Manipulation Servos (CH 0)
     public Servo flup; // (CH 1)
     public BNO055IMU imu;
 
     public OpenCvCamera camera;
-
-    int cameraMonitorViewId;
 
     public HardwareMap hwmap;
 
@@ -35,10 +35,15 @@ public class Ahi {
     public Ahi(HardwareMap hwmap) {
         this.hwmap = hwmap;
 
-        /*rightA = hwmap.get(DcMotor.class, "rightA");
-        rightB = hwmap.get(DcMotor.class, "rightB");
-        leftA = hwmap.get(DcMotor.class, "leftA");
-        leftB = hwmap.get(DcMotor.class, "leftB");
+        rightA = hwmap.get(DcMotorEx.class, "rightA");
+        rightB = hwmap.get(DcMotorEx.class, "rightB");
+        leftA = hwmap.get(DcMotorEx.class, "leftA");
+        leftB = hwmap.get(DcMotorEx.class, "leftB");
+
+        rightA.setPIDFCoefficients(DcMotor.RunMode.STOP_AND_RESET_ENCODER, new PIDFCoefficients(1, 1, 1, 1));
+        rightB.setPIDFCoefficients(DcMotor.RunMode.STOP_AND_RESET_ENCODER, new PIDFCoefficients(1, 1, 1, 1));
+        leftA.setPIDFCoefficients(DcMotor.RunMode.STOP_AND_RESET_ENCODER, new PIDFCoefficients(1, 1, 1, 1));
+        leftB.setPIDFCoefficients(DcMotor.RunMode.STOP_AND_RESET_ENCODER, new PIDFCoefficients(1, 1, 1, 1));
 
         rightA.setDirection(drivetrainReverse ? DcMotor.Direction.FORWARD : DcMotor.Direction.REVERSE);
         rightB.setDirection(drivetrainReverse ? DcMotor.Direction.FORWARD : DcMotor.Direction.REVERSE);
@@ -65,7 +70,7 @@ public class Ahi {
 
         capLift = hwmap.get(DcMotor.class, "capLift");
 
-        ddr = hwmap.get(CRServo.class, "ddr");*/
+        ddr = hwmap.get(CRServo.class, "ddr");
 
         BNO055IMU.Parameters parameters             = new BNO055IMU.Parameters();
         parameters.angleUnit                        = BNO055IMU.AngleUnit.RADIANS;
