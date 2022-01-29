1a652217969eb27a2905eed582be3626ac6f9d93
Dragons 10523 <92609241+Dragons10523@users.noreply.github.com>
2022-01-28 18:06:02 -0700

    Improved test text

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DriveDistTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DriveDistTest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.AbstractAutonomous;
 
@@ -11,37 +12,37 @@ public class DriveDistTest extends AbstractAutonomous {
         initialize();
         waitForStart();
 
-        telemetry.addLine("Driving forwards 6\"");
+        telemetry.addLine("Driving forwards 6");
         telemetry.update();
         if(protectedSleep(500)) return;
-        if(driveDist(6)) return;
-        telemetry.addLine("Desired angle achieved");
+        if(driveDist(48)) return;
+        telemetry.addLine("Desired distance achieved");
         telemetry.update();
         if(protectedSleep(500)) return;
-
-        telemetry.addLine("Driving forwards 12\"");
+/*
+        telemetry.addLine("Driving forwards 12");
         telemetry.update();
         if(protectedSleep(500)) return;
         if(driveDist(12)) return;
-        telemetry.addLine("Desired angle achieved");
+        telemetry.addLine("Desired distance achieved");
         telemetry.update();
         if(protectedSleep(500)) return;
 
-        telemetry.addLine("Driving backwards 6\"");
+        telemetry.addLine("Driving backwards 6");
         telemetry.update();
         if(protectedSleep(500)) return;
         if(driveDist(-6)) return;
-        telemetry.addLine("Desired angle achieved");
+        telemetry.addLine("Desired distance achieved");
         telemetry.update();
         if(protectedSleep(500)) return;
 
-        telemetry.addLine("Driving backwards 12\"");
+        telemetry.addLine("Driving backwards 12");
         telemetry.update();
         if(protectedSleep(500)) return;
         if(driveDist(-12)) return;
-        telemetry.addLine("Desired angle achieved");
+        telemetry.addLine("Desired distance achieved");
         telemetry.update();
-        if(protectedSleep(500)) return;
+        if(protectedSleep(500)) return;*/
 
         zero();
     }
