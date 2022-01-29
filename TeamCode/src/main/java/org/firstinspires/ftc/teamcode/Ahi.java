8a44b660202115721ea28e49a5d8e230f5947cfe
Dragons 10523 <92609241+Dragons10523@users.noreply.github.com>
2022-01-28 18:07:13 -0700

    Cleaned up code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ahi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ahi.java
@@ -40,11 +40,6 @@ public class Ahi {
         leftA = hwmap.get(DcMotorEx.class, "leftA");
         leftB = hwmap.get(DcMotorEx.class, "leftB");
 
-        /*rightA.setPIDFCoefficients(DcMotor.RunMode.STOP_AND_RESET_ENCODER, new PIDFCoefficients(1, 1, 1, 1));
-        rightB.setPIDFCoefficients(DcMotor.RunMode.STOP_AND_RESET_ENCODER, new PIDFCoefficients(1, 1, 1, 1));
-        leftA.setPIDFCoefficients(DcMotor.RunMode.STOP_AND_RESET_ENCODER, new PIDFCoefficients(1, 1, 1, 1));
-        leftB.setPIDFCoefficients(DcMotor.RunMode.STOP_AND_RESET_ENCODER, new PIDFCoefficients(1, 1, 1, 1));*/
-
         rightA.setDirection(drivetrainReverse ? DcMotor.Direction.FORWARD : DcMotor.Direction.REVERSE);
         rightB.setDirection(drivetrainReverse ? DcMotor.Direction.FORWARD : DcMotor.Direction.REVERSE);
         leftA.setDirection(drivetrainReverse ? DcMotor.Direction.REVERSE : DcMotor.Direction.FORWARD);
