62269adb1300948d67d5c7668d67e7f4db13f029
Dragons 10523 <92609241+Dragons10523@users.noreply.github.com>
2022-01-28 18:07:05 -0700

    Improved Driver

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AbstractDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AbstractDrive.java
@@ -6,8 +6,12 @@ package org.firstinspires.ftc.teamcode;
  * */
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public abstract class AbstractDrive extends Control {
+    boolean isPickup = false;
+    ElapsedTime elapsedTime;
+
     public void driveLoop() {
         double left = ahi.drivetrainReverse ? -gamepad1.right_stick_y : -gamepad1.left_stick_y;
         double right = ahi.drivetrainReverse ? -gamepad1.left_stick_y : -gamepad1.right_stick_y;
@@ -23,6 +27,8 @@ public abstract class AbstractDrive extends Control {
     public void run(FieldSide fieldSide) {
         waitForStart();
 
+        elapsedTime = new ElapsedTime();
+
         final double fieldDir = (fieldSide == FieldSide.RED ? -1.0 : 1.0);
 
         while(opModeIsActive()) {
@@ -30,18 +36,31 @@ public abstract class AbstractDrive extends Control {
 
             playDDR(gamepad2.right_bumper ? fieldDir : 0.0);
             playDDR(gamepad2.left_bumper ? -fieldDir : 0.0);
-            setFlup(gamepad2.left_trigger > .5);
-            runIntake((gamepad2.right_trigger * 0.5) - gamepad2.left_trigger);
+            setFlup(gamepad2.left_trigger < .5);
+            runIntake((gamepad2.dpad_left ? 1 : 0) - gamepad2.right_trigger);
             setLiftPower(-gamepad2.left_stick_y);
 
             if (gamepad2.x) {
-                armControl(ArmPosition.PICKUP);
+                armControl(ArmPosition.MED_FORE);
+                isPickup = true;
             } else if (gamepad2.a) {
                 armControl(ArmPosition.LOW);
+                isPickup = false;
             } else if (gamepad2.b) {
                 armControl(ArmPosition.MED);
+                isPickup = false;
             } else if (gamepad2.y) {
                 armControl(ArmPosition.HIGH);
+                isPickup = false;
+            }
+
+            if(isPickup && ahi.arm.getCurrentPosition() <= 650) {
+            } else {
+                elapsedTime.reset();
+            }
+
+            if(isPickup && elapsedTime.milliseconds() > 500) {
+                armControl(ArmPosition.PICKUP);
             }
 
             if (gamepad2.dpad_up) {
