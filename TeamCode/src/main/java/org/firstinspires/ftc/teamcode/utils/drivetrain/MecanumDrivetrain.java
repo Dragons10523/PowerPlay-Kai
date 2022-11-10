6f0fc039f9481755f6de4a596d9ecffe74a821f6
Dragons 10523 <92609241+Dragons10523@users.noreply.github.com>
2022-11-10 14:51:00 -0700

    Fixed the mecanum drivetrain to rotate properly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/drivetrain/MecanumDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/drivetrain/MecanumDrivetrain.java
@@ -6,6 +6,6 @@ import org.firstinspires.ftc.teamcode.utils.VecUtils;
 
 public class MecanumDrivetrain extends AbstractOmniDrivetrain {
     public MecanumDrivetrain(DcMotor frontLeft, DcMotor frontRight, DcMotor backLeft, DcMotor backRight) {
-        super(frontLeft, frontRight, backLeft, backRight, VecUtils.HALF_PI);
+        super(frontLeft, frontRight, backLeft, backRight, VecUtils.HALF_PI/2);
     }
 }
