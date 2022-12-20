076dd14bc6ea7649c7cb0ad0ba166a98a150bd3c
Dragons 10523 <92609241+Dragons10523@users.noreply.github.com>
2022-12-20 14:53:24 -0700

    Added autonomous decorations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoLeftImpl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoLeftImpl.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Left Auto")
 public class AutoLeftImpl extends Auto {
     @Override public void start() {
         auto(FieldSide.LEFT);
