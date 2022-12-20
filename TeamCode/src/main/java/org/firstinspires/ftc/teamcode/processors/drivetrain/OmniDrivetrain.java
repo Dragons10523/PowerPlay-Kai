e20214a8a02b57ef1712c7e5fc5d733ee176ae73
Dragons 10523 <92609241+Dragons10523@users.noreply.github.com>
2022-12-20 14:58:49 -0700

    Fixed omni drive direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processors/drivetrain/OmniDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processors/drivetrain/OmniDrivetrain.java
@@ -2,8 +2,10 @@ package org.firstinspires.ftc.teamcode.processors.drivetrain;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.processors.VecUtils;
+
 public class OmniDrivetrain extends AbstractOmniDrivetrain {
     public OmniDrivetrain(DcMotor frontLeft, DcMotor frontRight, DcMotor backLeft, DcMotor backRight) {
-        super(frontLeft, frontRight, backLeft, backRight, 0);
+        super(frontLeft, frontRight, backLeft, backRight, -VecUtils.HALF_PI/2);
     }
 }
