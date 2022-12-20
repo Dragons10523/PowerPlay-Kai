5bb0709692297dc4dd12b7996f3fb0ae8a053685
Dragons 10523 <92609241+Dragons10523@users.noreply.github.com>
2022-12-20 15:24:20 -0700

    Added drivetrain reversal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processors/drivetrain/AbstractDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processors/drivetrain/AbstractDrivetrain.java
@@ -13,6 +13,22 @@ public abstract class AbstractDrivetrain {
         driveMotors = new DcMotor[]{frontLeft, frontRight, backLeft, backRight};
     }
 
+    public void setDriveDirection(DcMotorSimple.Direction direction) {
+        switch(direction) {
+            case REVERSE:
+                driveMotors[0].setDirection(DcMotorSimple.Direction.FORWARD);
+                driveMotors[2].setDirection(DcMotorSimple.Direction.FORWARD);
+                driveMotors[1].setDirection(DcMotorSimple.Direction.REVERSE);
+                driveMotors[3].setDirection(DcMotorSimple.Direction.REVERSE);
+            case FORWARD:
+            default:
+                driveMotors[0].setDirection(DcMotorSimple.Direction.REVERSE);
+                driveMotors[2].setDirection(DcMotorSimple.Direction.REVERSE);
+                driveMotors[1].setDirection(DcMotorSimple.Direction.FORWARD);
+                driveMotors[3].setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+    }
+
     public void drive(double forwards, double turn) {
         driveMotors[0].setPower(forwards + turn);
         driveMotors[1].setPower(forwards - turn);
