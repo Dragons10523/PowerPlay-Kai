b2d9734c086f240a9d1e1135a8abbffac5e9b7ec
dragons <alaroboticsteam@gmail.com>
2022-12-24 16:47:04 -0700

    Added turntable controls to FixLift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FixLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FixLift.java
@@ -2,21 +2,42 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.processors.Control;
 
 @TeleOp(name = "Fix Lift")
 public class FixLift extends Control {
+    private float position = 0;
+    private ElapsedTime deltaTime;
+
     public void init() {
         super.init();
         kai.armLiftA.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         kai.armLiftB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         kai.liftExtension.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        deltaTime = new ElapsedTime();
+    }
+
+    public void init_loop() {
+        deltaTime.reset();
     }
 
     public void loop() {
         kai.armLiftA.setPower(-gamepad2.left_stick_y / 4);
         kai.armLiftB.setPower(-gamepad2.left_stick_y / 4);
         kai.liftExtension.setPower(-gamepad2.right_stick_y);
+
+        if(gamepad2.right_bumper) {
+            position += deltaTime.seconds()*300;
+        } else if(gamepad2.left_bumper) {
+            position -= deltaTime.seconds()*300;
+        }
+        deltaTime.reset();
+
+        kai.turntable.setTargetPosition((int)position);
+
+        telemetry.addData("Recorded Angle", kai.turntable.getCurrentPosition());
     }
 }
