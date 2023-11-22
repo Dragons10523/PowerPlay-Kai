9c191375660131a6b2592d93f0ecf860d583a4ff
Dragons 10523 <92609241+Dragons10523@users.noreply.github.com>
2023-11-22 11:09:45 -0700

    Imu Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mushu.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mushu.java
@@ -9,6 +9,7 @@ import com.arcrobotics.ftclib.hardware.GyroEx;
 import com.arcrobotics.ftclib.hardware.motors.CRServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
+import com.qualcomm.hardware.bosch.BHI260IMU;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -20,6 +21,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvInternalCamera;
@@ -30,12 +32,15 @@ public class Mushu extends Robot {
 
     public GamepadEx driverGamepad;
     public GamepadEx toolGamepad;
-    public IMU imu;
+    public BHI260IMU imu;
 
     public MecanumDrive mecanum;
     public Motor frontLeft, frontRight, backLeft, backRight;
     public Motor extakeArm, intakeArm;
     public CRServo intakeServo, omniServo;
+    double yaw;
+    double pitch;
+    double roll;
 
     public static Mushu GetInstance(CommandOpMode opMode) {
         if(instance == null)
@@ -63,16 +68,31 @@ public class Mushu extends Robot {
         intakeServo = new CRServo(hardwareMap, "intakeServo");
         omniServo = new CRServo(hardwareMap, "omniServo");
 
-        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters param;
+        param = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP
+                )
+        );
 
-        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                RevHubOrientationOnRobot.UsbFacingDirection.UP
-                );
+        // Initialize IMU directly
+        imu.initialize(
+                new IMU.Parameters(
+                        new RevHubOrientationOnRobot(
+                                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                                RevHubOrientationOnRobot.UsbFacingDirection.UP
+                        )
+                )
+        );
 
-        imu.initialize(new IMU.Parameters(orientationOnRobot));
+// Initialize IMU using Parameters
+        imu.initialize(param);
+        resetIMU();
+    
 
-
+// Now use these simple methods to extract each angle
+// (Java type double) from the object you just created:
 
 
         mecanum  = new MecanumDrive(frontLeft, frontRight, backLeft, backRight);
@@ -81,12 +101,11 @@ public class Mushu extends Robot {
         toolGamepad = new GamepadEx(opMode.gamepad2);
     }
     public double getHeading(){
-       Orientation Theta = imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
-        return Theta.firstAngle;
+        Orientation Theta = imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+       return Theta.firstAngle;
     }
     public void resetIMU(){
-        
-        imu.resetYaw();
+       imu.resetYaw();
     }
 
 }
