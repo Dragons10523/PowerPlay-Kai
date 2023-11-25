5232db8dd4b20ff47cbb3af29b47b4b20a969bf7
Dragons 10523 <92609241+Dragons10523@users.noreply.github.com>
2023-11-25 16:33:44 -0700

    Added IMU config data

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/IMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/IMU.java
@@ -9,7 +9,7 @@ public interface IMU extends com.qualcomm.robotcore.hardware.IMU {
     class Parameters extends com.qualcomm.robotcore.hardware.IMU.Parameters {
         public Parameters(ImuOrientationOnRobot imuOrientationOnRobot, OpMode opMode, Byte[] byteData) {
             super(imuOrientationOnRobot);
-            opMode.telemetry.addAction(() -> opMode.telemetry.addLine(Arrays.toString(byteData)));
+            opMode.telemetry.addAction(() -> opMode.telemetry.addLine(new String(byteData)));
         }
     }
 }
